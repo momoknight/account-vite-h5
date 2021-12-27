810105a36ba049ab4fb4a208b630f32f30567606
momoknight <yincheng.tao@outlook.com>
2021-12-27 22:13:27 +0800

    add dist file to github

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,7 +11,7 @@ module.exports = {
       host: '47.103.140.189',
       ref: 'origin/main',
       repo: 'git@github.com:momoknight/react-vite-h5.git',
-      path: '/react-vite-h5',
+      path: '/root/react-vite-h5',
       'post-deploy': 'git reset --hard && git checkout main && git pull && npm i --production=false && pm2 startOrReload ecosystem.config.js', // -production=false 下载全量包
       env: {
         NODE_ENV: 'production'
